b07169d49429493178f25b1cb9e147596e7735d7
Dhanvi Desai <dhanvidesai99@gmail.com>
2020-07-24 01:20:45 +0530

    Filtering data based on values given in command line

--- scarping.py
+++ scarping.py
@@ -4,12 +4,13 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support.ui import Select
 from selenium.webdriver.support.expected_conditions import presence_of_element_located
+import sys
 
 query_url = "https://www.pib.gov.in/allRel.aspx"
 
-month = "June"
-day = "All"
-year = "2020"
+month = sys.argv[2]
+day = sys.argv[1]
+year = sys.argv[3]
 
 executable_path = "C:\\Users\\Dhanvi\\Headless_Browsers\\chromedriver.exe"
 
@@ -18,19 +19,24 @@ chrome_options.add_argument("--headless")
 webdriver1 = webdriver.Chrome(executable_path=executable_path,options=chrome_options)
 print('Finished initalize')
 
-def select_month(wait,select,value):
+def select_value(wait,select,value):
     select.select_by_visible_text(value)
     wait.until(presence_of_element_located((By.CLASS_NAME,"content-area")))
-    div = driver.find_elements_by_class_name("content-area")[0]
-    return div
+    print('Finished selecting')
+    return driver
 
 
 with webdriver1 as driver:
     wait = WebDriverWait(driver,20)
     driver.get(query_url)
     wait.until(presence_of_element_located((By.CLASS_NAME,"content-area")))
-    select = Select(driver.find_elements_by_id("ContentPlaceHolder1_ddlMonth")[0])
-    div = select_month(wait,select,month)
+    select_day = Select(driver.find_elements_by_id("ContentPlaceHolder1_ddlday")[0])
+    driver = select_value(wait,select_day,day)
+    select_month = Select(driver.find_elements_by_id("ContentPlaceHolder1_ddlMonth")[0])
+    driver = select_value(wait,select_month,month)
+    select_year = Select(driver.find_elements_by_id("ContentPlaceHolder1_ddlYear")[0])
+    driver = select_value(wait,select_year,year)
+    div = driver.find_elements_by_class_name("content-area")[0]
     uls = div.find_elements_by_tag_name("ul")
     for ul in uls[1:3]:
         for a in ul.find_elements_by_tag_name("a"):
